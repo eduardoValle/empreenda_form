bff242cc54a92bdb251a197a1a15c58d0d918681
Luiz Eduardo <luizeduardovalle@gmail.com>
2017-04-11 14:57:22 -0300

    Primeiro commit

---
+++ empreenda_form.php
@@ -0,0 +1,15 @@
+<?php
+defined('ABSPATH') or die('No script kiddies please!');
+
+/*
+Plugin Name: Formulário de cadastro do Empreenda. Em Ação!
+Plugin URI:  https://www.empreendaemacao.com.br/
+Description: Formulário da cadasdro de participantes do programa empreenda!
+Version:     20160911
+Author:      Luiz Eduardo
+Author URI:  https://github.com/eduardoValle
+License:     GPL2
+License URI: https://www.gnu.org/licenses/gpl-2.0.html
+Text Domain: wporg
+Domain Path: /languages
+*/
